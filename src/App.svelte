e5cfc709159a0dd6d1ca44b0975061c57a047961
Ray Knight <array.knight+github@gmail.com>
2020-09-25 17:55:59 -0700

    feat: 🎸 bind pixi to app

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,55 @@
 <script lang="ts">
-    console.log('hello')
+    import { Application } from '@pixi/app'
+    import { onMount } from 'svelte'
+
+    const app = new Application({
+        antialias: true,
+    })
+
+    app.renderer.autoResize = true
+
+    function bindApp(node) {
+        node.appendChild(app.view)
+
+        return {
+            destroy() {
+                node.removeChild(app.view)
+            },
+        }
+    }
+
+    function resize() {
+        app.renderer.resize(
+            app.view.parentNode.clientWidth,
+            app.view.parentNode.clientHeight,
+        )
+    }
+
+    onMount(() => {
+        resize()
+
+        window.addEventListener('resize', resize)
+
+        return () => {
+            window.removeEventListener('resize', resize)
+        }
+    })
 </script>
 
-<div>Hello</div>
+<div class="App" use:bindApp></div>
+
+<style lang="scss">
+    :global(html, body) {
+        width: 100%;
+        height: 100%;
+    }
+
+    .App {
+        &,
+        canvas {
+            width: 100%;
+            height: 100%;
+            display: block;
+        }
+    }
+</style>
